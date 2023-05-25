be0292ab4cf96be2a1cdf3c8ca154c28635dae6f
Thomas LOPES <thomas.lopes@wildcodeschool.com>
2023-05-25 15:09:31 +0200

    correction

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const Fighter = require("./src/Fighter.js");
+const Weapon = require("./src/Weapon.js");
+const Shield = require("./src/Shield.js");
 
 /** Create Heracles  */
 const heracles = new Fighter("🧔 Heracles", 20, 6);
+const sword = new Weapon("Epée", 10);
+const shield = new Shield(10);
+heracles.weapon = sword;
+heracles.shield = shield;
 
 /** Create the opponent  */
 const boar = new Fighter("🐗 Erymanthian Boar", 25, 12);
@@ -25,3 +31,53 @@ const score = (fighter1, fighter2) => {
     loser: fighter1
   };
 };
+
+// Let's fight !!!
+function fight(fighter1, fighter2) {
+  {
+    let round = 0;
+
+    while (fighter1.isAlive() && fighter2.isAlive()) {
+      console.log(`🕛 Round #${round}`);
+
+      fighter1.fight(fighter2);
+      console.log(roundDisplay(fighter1, fighter2));
+
+      fighter2.fight(fighter1);
+      console.log(roundDisplay(fighter2, fighter1));
+
+      round++;
+    }
+
+    const result = score(fighter1, fighter2);
+
+    console.log(`💀 ${result.loser.name} is dead`);
+    console.log(`🏆 ${result.winner.name} wins (💙 ${result.winner.life} )`);
+    return result.winner;
+  }
+
+}
+
+const scoring = {
+  heraclesWins: 0,
+  boarWins: 0
+}
+for (let i = 0; i < 10; i++) {
+  const heracles = new Fighter("🧔 Heracles", 20, 6);
+  const sword = new Weapon("Epée", 10);
+  const shield = new Shield(10);
+  heracles.weapon = sword;
+  heracles.shield = shield;
+
+  /** Create the opponent  */
+  const boar = new Fighter("🐗 Erymanthian Boar", 25, 12);
+
+  let res = fight(heracles, boar);
+  if (res.name === heracles.name) {
+    scoring.heraclesWins++;
+  } else {
+    scoring.boarWins++;
+  }
+}
+
+console.log(scoring);
--- src/Fighter.js
+++ src/Fighter.js
@@ -6,14 +6,16 @@ class Fighter {
         this.strength = strength;
         this.dexterity = dexterity;
         this.life = MAX_LIFE;
+        this.weapon = null;
+        this.shield = null;
     }
 
 
     // Launch a fight
     fight(defender) {
-        const attackPoints = this.getRandomInt(this.strength);
+        const attackPoints = this.getRandomInt(this.getDamage());
 
-        const damages = Math.max(attackPoints - defender.dexterity, 0);
+        const damages = Math.max(attackPoints - this.getDefense(), 0);
 
         defender.life = Math.max(defender.life - damages, 0);
     }
@@ -28,6 +30,25 @@ class Fighter {
     // Determine if a fighter is still alive
     isAlive() {
         return this.life > 0;
+    }
+
+    getDamage(){
+        // if(this.weapon){
+        //     return this.strength + this.weapon.damage;
+        // }else{
+        //     return this.strength;
+        // }
+
+        return this.weapon ? this.strength + this.weapon.damage : this.strength;
+    }
+
+    getDefense(){
+        // if(this.shield){
+        //     return this.dexterity + this.shield.protection;
+        // }else{
+        //     return this.dexterity;
+        // }
+        return this.shield ? this.dexterity + this.shield.protection : this.dexterity;
     }
 }
 
